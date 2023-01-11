ad5723593dceb920e5e9c700c2e13e7223891c14
rushita11 <rushitathummar123@gmail.com>
2023-01-11 09:24:44 +0530

    11-1-222

--- src/container/auth/Auths.js
+++ src/container/auth/Auths.js
@@ -37,6 +37,7 @@ function Signup(props) {
                                     :
                                     null
                             }
+                            
                             {
                                 reset ?
                                     <Col>
