8705c5f1afa33e3c65411d94b52a9b34b848ba4f
rushita11 <rushitathummar123@gmail.com>
2023-01-25 09:18:03 +0530

    25-1-2023(form validation)

--- src/admin/Container/Medicine/Medicins.js
+++ src/admin/Container/Medicine/Medicins.js
@@ -5,18 +5,20 @@ import MedicineModal from '../MedicineModal';
 
 function Medicins(props) {
     const [open,setOpen] = React.useState(false)
-    const openModal = ()=>{
+    // const openModal = ()=>{
 
-    }
+    // }
     return (
-        <div>
+        <>
+        <div className='Text d-flex justify-content-between'>
             <h1>Medicine</h1>
-            <div className="text-end"><button className='appointment-btn mt-3 border-0' onClick={()=>setOpen(true)}>Open Modal</button></div>
+            <div className="text-end"><button className='appointment-btn border-0 m-0 ' onClick={()=>setOpen(true)}>Open Modal</button>
             <MedicineModal 
                 open={open}
                 handleClose={()=>setOpen(false)}
-            />
-        </div>
+            /></div>
+            </div>
+        </>
     );
 }
 
--- src/admin/Container/MedicineModal.js
+++ src/admin/Container/MedicineModal.js
@@ -1,19 +1,10 @@
 import { CloseOutlined } from '@mui/icons-material';
-import { Box, Modal, Typography } from '@mui/material'
+import { Box, Modal, Typography } from '@mui/material';
+import TextField from '@mui/material/TextField';
 import React from 'react'
 
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 500,
-    bgcolor: 'background.paper',
-   
-    boxShadow: 24,
-    p: 4,
-  };
-const MedicineModal = ({open,handleClose}) => {
+
+const MedicineModal = ({ open, handleClose }) => {
     return (
         <div>
             <Modal
@@ -22,59 +13,64 @@ const MedicineModal = ({open,handleClose}) => {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <Box sx={style}>
-                    <div className='closeIcon'><CloseOutlined onClick={handleClose}/></div>
-                <div className='Signup_form'>
-                    <form method="post" role="form" className="php-email-form" >
-                        <div className="row">
-                            <div className="col-md-6 form-group">
-                                <input
-                                    type="text"
-                                    name="name"
-                                    className="form-control"
-                                    id="name"
-                                    placeholder="Your Name"
-                                     />
-                            </div>
-                            <div className="col-md-6 form-group mt-3 mt-md-0">
-                                <input
-                                    type="email"
-                                    className="form-control"
-                                    name="email" id="email"
-                                    placeholder="Your Email"
-                                />
-                            </div>
-                            <div className="col-md-6 form-group mt-3 mt-md-0">
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    name="mobile" id="number"
-                                    placeholder="Your contact"
-                                />
-                            </div>
-                            <div className="col-md-6 form-group mt-3 mt-md-0">
-                                <input
-                                    type="radio"
+                <Box className="modal">
+                    <div className='closeIcon'><CloseOutlined onClick={handleClose} /></div>
+                    <div className='Signup_form'>
+                        <form method="post" role="form" className="php-email-form" >
+                            <div className="row">
+                                <div className="col-md-6 form-group">
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        className="form-control"
+                                        id="medicine"
+                                        placeholder="Medicine Name"
+                                    />
+                                    <TextField
+                                        error
+                                        id="outlined-error"
+                                        label="Error"
+                                        defaultValue="Name"
+                                    />
+                                </div>
+                                <div className="col-md-6 form-group mt-3 mt-md-0">
+                                    <input
+                                        type="email"
+                                        className="form-control"
+                                        name="email" id="email"
+                                        placeholder="Your Email"
+                                    />
+                                </div>
+                                <div className="col-md-6 form-group mt-3 mt-md-0">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        name="mobile" id="number"
+                                        placeholder="Your contact"
+                                    />
+                                </div>
+                                <div className="col-md-6 form-group mt-3 mt-md-0">
+                                    <input
+                                        type="radio"
+                                        name="gender"
+                                    /> Male
+                                    <input
+                                        type="radio"
+                                        className='ms-3'
+                                        name="gender"
 
-                                    name="gender"
-                                /> Male
-                                <input
-                                    type="radio"
-                                    className='ms-3'
-                                    name="gender"
-
-                                /> Female
+                                    /> Female
+                                </div>
                             </div>
-                        </div>
-                        <div className="form-group mt-3">
-                            <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" />
-                        </div>
-                        <div className="form-group mt-3">
-                            <textarea className="form-control" name="message" rows={5} placeholder="Message" defaultValue={""} />
-                        </div>
-                    </form>
-                    <div className="text-center "><button onClick={handleClose} className='appointment-btn mt-3 border-0' >Send Message</button></div>
-                </div>
+                            <div className="form-group mt-3">
+                                <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" />
+                            </div>
+                            <div className="form-group mt-3">
+                                <textarea className="form-control" name="message" rows={5} placeholder="Message" defaultValue={""} />
+                            </div>
+                        </form>
+                        <div className="text-center "><button type='submit' className='appointment-btn mt-3 border-0' >Save</button></div>
+                    </div>
                 </Box>
             </Modal>
         </div>
